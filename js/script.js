e9d44bd8e2c469eb8b6c268c90d01397776f8725
Zelkhat <zelkhat@gmail.com>
2017-06-30 14:06:23 -0400

    start

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function() {
+
+	$('.menu a').on('click', function(e) {
+		e.preventDefault();
+
+		// highlight current menu
+		$('.menu a').removeClass('active').filter(this).addClass('active');
+
+		var $selector = $(this).attr('href');
+		var $h = $($selector);
+		
+		$('html, body').animate({
+			// menu is fixed, margin-top for content is 80px
+			scrollTop: $h.offset().top - 80
+		}, 800);
+
+	});
+
+	$btnScrollToTop = $('.scrollToTop');
+
+	$(window).scroll(function() {
+		//$btnScrollToTop = $('.scrollToTop');
+		if ($(document).scrollTop() > 500) {
+			$btnScrollToTop.css('display', 'block');
+		} else {
+			$btnScrollToTop.css('display', 'none');
+		}
+	});
+
+	$btnScrollToTop.hover(function() {
+		$(this).css('background', 'rgba(51, 51, 51, 1)');
+	}, function() {
+		$(this).css('background', 'rgba(51, 51, 51, 0.5)');
+	}).on('click', function() {
+		$('html, body').animate({
+			// menu is fixed, margin-top for content is 80px
+			scrollTop: $('#about').offset().top - 80
+		}, 800); 
+	});
+
+});
